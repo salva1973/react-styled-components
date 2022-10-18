6d394712b48041b16b9b6bbb6149a4e084540e6d
Salvatore Vivolo <3092390+salva1973@users.noreply.github.com>
2022-10-18 23:49:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# react-styled-components
+
+## Basic styling options in React projects
+
+- inline styles
+- global css (styles in a stylesheet)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    // Inline style
+    <div style={{ padding: '2rem' }}>
+      {/* Global Stylesheet */}
+      <h1 className="title">styled components</h1>
+      <button className="btn">click me</button>
+    </div>
+  )
+}
+
+export default App
